fd43d8f9cc039101d549485ef0599773c6c92cf6
Charsi420 <abdul.wahib51@gmail.com>
2023-01-05 00:36:46 +0500

    a

--- next-app/components/Layout.js
+++ next-app/components/Layout.js
@@ -2,7 +2,7 @@ import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
+    <div>
       <main className={styles.main}>{children}</main>
     </div>
   );
