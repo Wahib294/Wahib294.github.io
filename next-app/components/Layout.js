ea210d5d76165f0ab5818cafe43a8523b6f0e765
Charsi420 <abdul.wahib51@gmail.com>
2023-01-05 00:20:00 +0500

    Update Layout.js

--- next-app/components/Layout.js
+++ next-app/components/Layout.js
@@ -2,8 +2,9 @@ import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <main>{children}</main>
+    <div className={styles.content}>
+      <main className={styles.main}>{children}</main>
     </div>
   );
 };
+export default Layout;
