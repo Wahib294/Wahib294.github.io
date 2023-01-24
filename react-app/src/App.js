3aa647febfb70fdcaa52e5ddbd649457547e8360
Charsi420 <abdul.wahib51@gmail.com>
2023-01-25 01:37:57 +0500

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 function App() {
   return (
     <>
-      <Link href="./login">Login</Link>
+      <Link to="/login">Login</Link>
       <div>Abdul Wahib</div>
     </>
   );
