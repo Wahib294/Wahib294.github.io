12a7205d995655680bbe909d4babcfb0e0b0c609
Charsi420 <abdul.wahib51@gmail.com>
2023-01-24 22:08:06 +0500

    Update index.js

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,4 +1,18 @@
 import Head from "next/head";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://wahib:wahibwahib2@cluster0.waqyd8i.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("chess").collection("user");
+  client.close();
+});
 
 export default function Home() {
   return (
@@ -6,7 +20,7 @@ export default function Home() {
       <Head>
         <title>Next App</title>
       </Head>
-      <h1>Welcome to NEXT</h1>
+      <h1>{collection}</h1>
     </div>
   );
 }
