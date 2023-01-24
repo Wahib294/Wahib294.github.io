08dfa65b2816631fad460713bfa2976958eaec49
Charsi420 <abdul.wahib51@gmail.com>
2023-01-25 01:27:09 +0500

    a

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "./login";
+import * as React from "react";
 import { Link } from "react-router-dom";
 var rating = Array(5).fill(500);
 function App() {
   return (
     <>
       <Link href="/login">Login</Link>
-      <div className="App">
+      <div>
         Abdul Wahib
         {rating[0]}
       </div>
