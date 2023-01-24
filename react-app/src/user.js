2c29b7a5ff45be8a66e6f1efdd33aad3cca63778
Charsi420 <abdul.wahib51@gmail.com>
2023-01-25 02:56:37 +0500

    Update user.js

--- react-app/src/user.js
+++ react-app/src/user.js
@@ -1,24 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const AsyncAwait = () => {
-  const [users, setUsers] = useState([]);
-
-  const fetchData = async () => {
-    const response = await fetch(
-      "https://chess-70d1d-default-rtdb.asia-southeast1.firebasedatabase.app/user"
-    );
-    const data = await response.json();
-    setUsers(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const response = fetch(
+    "https://chess-70d1d-default-rtdb.asia-southeast1.firebasedatabase.app/user.json"
+  );
+  const data = response.json();
   return (
     <div>
-      {users.length > 0 && (
+      {data.length > 0 && (
         <ul>
-          {users.map((user) => (
+          {data.map((user) => (
             <li key={user.id}>{user.name}</li>
           ))}
         </ul>
