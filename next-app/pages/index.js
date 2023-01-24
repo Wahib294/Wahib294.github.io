c452a7de3fb5695e91ebfdb16982db3f9062a5fe
Charsi420 <abdul.wahib51@gmail.com>
2023-01-24 23:24:44 +0500

    Update index.js

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,19 +1,5 @@
 import Head from "next/head";
 
-import { MongoClient, ServerApiVersion } from "mongodb";
-const uri =
-  "mongodb+srv://wahib:<password>@cluster0.waqyd8i.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 export default function Home() {
   return (
     <div>
