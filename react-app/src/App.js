75e14ceeb3746ac6b5bdba12e01bcbc1e2275e18
Charsi420 <abdul.wahib51@gmail.com>
2023-01-25 01:30:37 +0500

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,10 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-var rating = Array(5).fill(500);
 function App() {
   return (
     <>
-      <Link href="/login">Login</Link>
-      <div>
-        Abdul Wahib
-        {rating[0]}
-      </div>
+      <Link href="./login">Login</Link>
+      <div>Abdul Wahib</div>
     </>
   );
 }
