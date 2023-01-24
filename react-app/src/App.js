9282aaa1d0f5a767254012845565bc929220467d
Charsi420 <abdul.wahib51@gmail.com>
2023-01-25 02:13:47 +0500

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
             </>
           }
         />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<login />} />
       </Routes>
     </BrowserRouter>
   );
