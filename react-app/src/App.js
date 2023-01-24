4f8e38d560972cc9655df7c03a49308773970872
Charsi420 <abdul.wahib51@gmail.com>
2023-01-24 23:47:16 +0500

    a

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-var rating = 34;
+var rating = Array(5).fill(500);
 
 function App() {
-  return <div className="App">Abdul Wahib {rating}</div>;
+  return (
+    <div className="App">
+      Abdul Wahib
+      <button onClick={increment}>+</button>
+      {rating[0]}
+      <button onClick={decrement}>-</button>
+    </div>
+  );
 }
 
 export default App;
