6bad7aa5147c54d2b7bb8f2a3a5ff04144e9d777
Charsi420 <abdul.wahib51@gmail.com>
2023-01-24 23:07:24 +0500

    a

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -2,14 +2,15 @@ import Head from "next/head";
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://wahib:wahibwahib2@cluster0.waqyd8i.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://wahib:<password>@cluster0.waqyd8i.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 client.connect((err) => {
-  const collection = client.db("chess").collection("user");
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
   client.close();
 });
 
