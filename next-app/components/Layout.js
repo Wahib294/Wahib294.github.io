074cef705551c4762cecd708c86abce30c17b5a6
Charsi420 <abdul.wahib51@gmail.com>
2023-01-24 22:31:53 +0500

    Update Layout.js

--- next-app/components/Layout.js
+++ next-app/components/Layout.js
@@ -1,5 +1,23 @@
 import styles from "../styles/Layout.module.css";
 import Nav from "./Nav";
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+  try {
+    const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+    const collection = database.collection(process.env.MONGODB_COLLECTION);
+    // Function logic here ...
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
+
 const Layout = ({ children }) => {
   return (
     <>
